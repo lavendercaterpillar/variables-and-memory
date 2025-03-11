43bb82ac496aff4919231fb25a62770478d0bdf8
lavendercaterpillar <elhamalavi.f@gmail.com>
2025-03-11 16:59:10 -0400

    Modified challenge 3 & 4

--- challenges/scope_challenges.py
+++ challenges/scope_challenges.py
@@ -10,14 +10,14 @@ def challenge_02():
 
 def challenge_03():
     # 3
+    inner_value = 42
     print(f"The value from outside is: {inner_value}")
 
-    inner_value = 42
 
 def challenge_04():
     # 4
+    outer_value = 42
     print(f"The value from outside is: {outer_value}")
 
-    outer_value = 42
 
 outer_value = 1729
